a99f801821d9b5ebe9783b1c50a82ed7734264d5
Sean <devlinsf@devlinsolutions.com>
2013-01-28 17:37:11 -0500

    Added lots of pruning functions to the search space

--- src/factorial/core.clj
+++ src/factorial/core.clj
@@ -69,10 +69,106 @@
   (for [x seq y seq op [+ - *]]
        (conj seq (op x y))))
 
+(defn prep-mult-seq [coll]
+  (let [ blacklist-nums (reduce (fn [accum [k v]]
+                    (if (some #{k} coll)
+                      (apply conj accum v)
+                      accum))
+                #{1 2} {
+                12 [3]
+                144 [12]
+                11 [11]
+                13 [13]
+                17 [17]
+                19 [19]
+                23 [23]
+                29 [29]
+                31 [31]
+                37 [37]
+                143 [144 143]
+                })
+        ]
+  (remove blacklist-nums coll)))
+
+(defn brute-add-2 [seq]
+  (concat
+    (for [x seq y seq]
+         (conj seq (+ x y)))
+    (for [x seq y seq :when (> x y)]
+         (conj seq (- x y)))
+    (let [mult-seq (prep-mult-seq seq)]
+      (for [x mult-seq y mult-seq]
+           (conj seq (* x y)))
+      )))
+
+(defn duplicates? [coll]
+  (not (= (count (distinct coll))
+          (count coll))))
+
+(def loser-numbers
+     #{8 9 10 256 512 1024})
+
+(def winners {
+     6 #{12}
+     7 #{144}
+     8 #{143}
+     9 #{35}
+     })
+
+(defn require-all-winners [n]
+  (let [must-haves (reduce (& distinct concat) 
+                           (map winners (range 1 (inc n))))]
+    (fn [coll]
+        (every? (set coll) must-haves))))
+
+((require-all-winners 6) [1])
+
+(defn sprawl [[n sets]]
+  [(inc n) ((& 
+                 set 
+                 (p map (& first second))
+                 (p group-by sort) 
+                 (p remove duplicates?) 
+                 (p remove (p some loser-numbers)) 
+                 (p filter (require-all-winners n)) 
+                 (p remove (p some neg?)) 
+                 (p remove (p some zero?)) 
+                 (p apply concat) 
+                 (p map brute-add-2))
+            sets)])
+
+(defn level [n seed]
+  (second (last (take n (iterate sprawl [2 seed]))))
+  )
+
+(level 10 #{[1]})
+(count (level 8 #{[1]}))
+(count (level 9 #{[1]}))
+(count (level 10 #{[1]}))
+(count (level 11 #{[1]}))
+(take 20 (level 11 #{[1]}))
+
+(apply do-it 13 (map rest (level 9 #{[1]})))
+(apply do-it 14 (map rest (level 9 #{[1]})))
+(apply do-it 15 (map rest (level 9 #{[1]})))
+(apply do-it 16 (map rest (level 9 #{[1]})))
+(apply do-it 17 (map rest (level 10 #{[1]})))
+(apply do-it 18 (map rest (level 10 #{[1]})))
+(apply do-it 19 (map rest (level 11 #{[1]})))
+(apply do-it 14 (level 8 #{[1]}))
+(apply do-it 13 (level 7 #{[1]}))
+
+(count (last (take 9 (iterate sprawl #{[1]}))))
+(last (take 8 (iterate sprawl #{[1]})))
+(take 50 (last (take 8 (iterate sprawl #{[1]}))))
+
+(count (last (take 5 (iterate (& set (p remove (p some zero?)) (p apply concat) (p map brute-add)) #{[1]}))))
+(count (last (take 6 (iterate (& set (p remove (p some zero?)) (p apply concat) (p map brute-add)) #{[1]}))))
+       
+(last (take 3 (iterate (& (p apply concat) (p map brute-add)) #{[1]})))
+(count (last (take 5 (iterate (& (p apply concat) (p map brute-add)) #{[1]}))))
 (count (last (take 2 (iterate (& set (p apply concat) (p map brute-add)) #{[1]}))))
 (count (last (take 3 (iterate (& set (p apply concat) (p map brute-add)) #{[1]}))))
-(count (last (take 4 (iterate (& set (p apply concat) (p map brute-add)) #{[1]}))))
-(count (last (take 5 (iterate (& set (p apply concat) (p map brute-add)) #{[1]}))))
 (count (last (take 6 (iterate (& set (p apply concat) (p map brute-add)) #{[1]}))))
 (count (last (take 7 (iterate (& set (p apply concat) (p map brute-add)) #{[1]}))))
 (count (last (take 8 (iterate (& set (p apply concat) (p map brute-add)) #{[1]}))))
